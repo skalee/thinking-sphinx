d96a25892367d6641a4024a4b7497be2b6f1a5c6
Jaroslaw Skrzypek <hawk@zenbe.com>
2010-01-15 11:47:39 +0100

    enabled updates for test env

--- lib/thinking_sphinx.rb
+++ lib/thinking_sphinx.rb
@@ -124,7 +124,7 @@ module ThinkingSphinx
   def self.updates_enabled?
     if Thread.current[:thinking_sphinx_updates_enabled].nil?
       Thread.current[:thinking_sphinx_updates_enabled] = (
-        ThinkingSphinx::Configuration.environment != "test"
+        return true
       )
     end
     
